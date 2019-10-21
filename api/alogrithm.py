31904271c43ef60e335b3bc485e303c953ea01de
491216697@qq.com <491216697@qq.com>
2019-10-21 15:46:46 +0800

    v6

--- api/alogrithm.py
+++ api/alogrithm.py
@@ -1,5 +1,6 @@
 import os
 import copy
+import time
 
 
 class Constant:
@@ -33,28 +34,23 @@ class Gobang(Constant):
         self.__History = []
         self.__Top = 0
 
-    def __get_neighbor_points(self, point=(0, 0), the_map=None, neighbor=None, deep=2):
+    def __get_neighbor_points(self, point=(0, 0), the_map=None, neighbor=None):
         if neighbor is None:
             neighbor = self.__Neighbors
         if the_map is None:
             the_map = self.__Map
         for i in self.directions:
-            for j in range(1, 1 + deep):
-                x = point[0] + i[0] * j
-                y = point[1] + i[1] * j
-                if 0 <= x <= 14 and 0 <= y <= 14:
-                    if the_map[x][y] is self.Blank:
-                        if not (j, x, y) in neighbor:
-                            neighbor.append((j, x, y))
-                    else:
-                        if (j, x, y) in neighbor:
-                            neighbor.pop(neighbor.index((j, x, y)))
-        x = point[0]
-        y = point[1]
-        for j in range(1, 1 + deep):
-            if (j, x, y) in neighbor:
-                neighbor.pop(neighbor.index((j, x, y)))
-        neighbor.sort()
+            x = point[0] + i[0]
+            y = point[1] + i[1]
+            if 0 <= x <= 14 and 0 <= y <= 14:
+                if the_map[x][y] is self.Blank:
+                    if not (x, y) in neighbor:
+                        neighbor.append((x, y))
+                else:
+                    if (x, y) in neighbor:
+                        neighbor.pop(neighbor.index((x, y)))
+        if point in neighbor:
+            neighbor.pop(neighbor.index(point))
         return neighbor
 
     def __put_chess(self, the_map, neighbor, player, point, max_min, scores):
@@ -64,16 +60,16 @@ class Gobang(Constant):
             the_map[x][y] = self.WhiteChess
         else:
             the_map[x][y] = self.BlackChess
-        if x <= max_min[0]:
+        if x < max_min[0]:
             max_min[0] = x - 1
-        if y <= max_min[1]:
+        if y < max_min[1]:
             max_min[1] = y - 1
-        if x >= max_min[2]:
+        if x > max_min[2]:
             max_min[2] = x + 1
-        if y >= max_min[3]:
+        if y > max_min[3]:
             max_min[3] = y + 1
         if neighbor is not None:
-            neighbor = self.__get_neighbor_points(point=point, the_map=the_map, neighbor=neighbor, deep=1)
+            neighbor = self.__get_neighbor_points(point=point, the_map=the_map, neighbor=neighbor)
         scores = self.__get_scores(point=point, the_map=the_map, max_min=max_min, scores=scores)
         return the_map, neighbor, max_min, scores
 
@@ -106,6 +102,15 @@ class Gobang(Constant):
             return chess
         else:
             return 0
+
+    def is_win(self, point, max_min, the_map):
+        points = self.__clu_start_point(point=point, max_min=max_min)
+        for i in range(4):
+            t = self.__is_win(max_min=max_min, the_map=the_map, start_point=points[i],
+                              direction=self.directions[i])
+            if t is not self.Blank:
+                return t
+        return 0
 
     def __scan_map(self, max_min, the_map, start_point, direction):
         x = start_point[0]
@@ -223,15 +228,10 @@ class Gobang(Constant):
             points.append((x + self.directions[i][0], y + self.directions[i][1]))
         return points
 
-    def __get_chess(self, the_map, neighbor, scores, player, max_min, alpha, beta, deep, point):
+    def __get_chess(self, the_map, neighbor, scores, player, max_min, alpha, beta, deep, point=None):
         t = self.Blank
         if point is not None:
-            points = self.__clu_start_point(point=point, max_min=self.__max_min)
-            for j in range(4):
-                t = self.__is_win(max_min=self.__max_min, the_map=self.__Map, start_point=points[j],
-                                  direction=self.directions[j])
-                if t is not self.Blank:
-                    break
+            t = self.is_win(point=point, max_min=max_min, the_map=the_map)
         if deep <= 0 or t is not self.Blank:
             score = self.__get_total_score(scores=scores)
             if self.__Player is True:
@@ -243,26 +243,24 @@ class Gobang(Constant):
         for i in neighbor:
             t_the_map, t_neighbor, t_max_min, t_scores = self.__put_chess(the_map=copy.deepcopy(the_map),
                                                                           neighbor=copy.deepcopy(neighbor),
-                                                                          player=player, point=(i[1], i[2]),
+                                                                          player=player, point=(i[0], i[1]),
                                                                           max_min=copy.deepcopy(max_min),
                                                                           scores=copy.deepcopy(scores))
-
             score, point = self.__get_chess(the_map=t_the_map, neighbor=t_neighbor, scores=t_scores,
                                             player=not player, max_min=t_max_min, alpha=alpha,
-                                            beta=beta,
-                                            deep=deep - 1, point=(i[1], i[2]))
+                                            beta=beta, deep=deep - 1, point=(i[0], i[1]))
             if player is not self.__Player:
                 if score > alpha:
                     alpha = score
-                    point = (i[1], i[2])
-                if beta <= alpha:
-                    return alpha, point
+                    point = (i[0], i[1])
+                    if score > beta:
+                        break
             else:
                 if score < beta:
                     beta = score
-                    point = (i[1], i[2])
-                if beta <= alpha:
-                    return beta, point
+                    point = (i[0], i[1])
+                    if score < alpha:
+                        break
         if player is not self.__Player:
             return alpha, point
         else:
@@ -278,23 +276,14 @@ class Gobang(Constant):
                                                                                            scores=self.__Scores)
             self.__History.append(point)
             self.__Top += 1
-            points = self.__clu_start_point(point=point, max_min=self.__max_min)
-            for i in range(4):
-                t = self.__is_win(max_min=self.__max_min, the_map=self.__Map, start_point=points[i],
-                                  direction=self.directions[i])
-                if t is not self.Blank:
-                    return t
         self.__Turn = not self.__Turn
-        return 0
+        return self.is_win(point=point, max_min=self.__max_min, the_map=self.__Map)
 
     def get_chess(self, player):
         score, point = self.__get_chess(the_map=self.__Map, neighbor=self.__Neighbors,
-                                        scores=self.__Scores,
-                                        player=player, max_min=self.__max_min, alpha=-self.Inf,
-                                        beta=self.Inf,
-                                        deep=2, point=None)
-        self.put_chess(player=player, timeout=False, point=point)
-        return point
+                                        scores=self.__Scores, player=player, max_min=self.__max_min, alpha=-self.Inf,
+                                        beta=self.Inf, deep=3)
+        return self.put_chess(player=player, timeout=False, point=point), point
 
     def save(self, path, name):
         if not os.path.exists(path):
